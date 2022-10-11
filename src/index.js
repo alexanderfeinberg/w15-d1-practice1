988ff4b52363f221788fa96b38c5e309b13e1b01
Alexander Feinberg <feinberg.alexander@gmail.com>
2022-10-11 16:41:52 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import configureStore from "./store";
 import "./index.css";
-import { fruitReducer } from "./store/fruitReducer";
-import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
